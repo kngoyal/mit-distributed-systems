271aedc7b890cf46932bc86d154ec893799a0bc8
kngoyal <kngoyal.cs@gmail.com>
2021-04-23 12:31:18 -0400

    add logging using logrus package

--- 6.824/src/go.mod
+++ 6.824/src/go.mod
@@ -1,3 +1,5 @@
 module github.com/mit-distributed-systems/6.824/src
 
 go 1.16
+
+require github.com/sirupsen/logrus v1.8.1 // indirect
--- 6.824/src/main/mrcoordinator.go
+++ 6.824/src/main/mrcoordinator.go
@@ -10,16 +10,32 @@ package main
 //
 
 import (
-	"fmt"
 	"os"
 	"time"
 
 	mr "github.com/mit-distributed-systems/6.824/src/mr"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	lvl, ok := os.LookupEnv("GO_LOG_LEVEL")
+	// LOG_LEVEL not set, let's default to info
+	if !ok {
+		lvl = "info"
+	}
+	// parse string, this is built-in feature of logrus
+	ll, err := log.ParseLevel(lvl)
+	if err != nil {
+		ll = log.InfoLevel
+	}
+	// set global log level
+	log.SetLevel(ll)
+}
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: mrcoordinator inputfiles...\n")
+		log.Error(os.Stderr, "Usage: mrcoordinator inputfiles...\n")
 		os.Exit(1)
 	}
 
