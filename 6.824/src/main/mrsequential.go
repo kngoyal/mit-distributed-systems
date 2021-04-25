de72bf6c7d86036f1090ea7728d7d7f2b727d3f3
kngoyal <kngoyal.cs@gmail.com>
2021-04-25 18:57:21 -0400

    use channel implementaton

--- 6.824/src/main/mrsequential.go
+++ 6.824/src/main/mrsequential.go
@@ -14,7 +14,7 @@ import (
 	"plugin"
 	"sort"
 
-	mr "github.com/mit-distributed-systems/6.824/src/mrconcurrent"
+	mr "github.com/mit-distributed-systems/6.824/src/mr/channel"
 )
 
 // for sorting by key.
